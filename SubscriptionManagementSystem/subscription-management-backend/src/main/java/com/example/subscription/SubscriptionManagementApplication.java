58d08b33d5cdb8edd60104a9328157c94af580ec
AV <67626681+non-sequitor@users.noreply.github.com>
2025-09-13 10:40:05 +0530

    Update SubscriptionManagementApplication.java

--- SubscriptionManagementSystem/subscription-management-backend/src/main/java/com/example/subscription/SubscriptionManagementApplication.java
+++ SubscriptionManagementSystem/subscription-management-backend/src/main/java/com/example/subscription/SubscriptionManagementApplication.java
@@ -1,1 +1,13 @@
+package com.example.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionManagementApplication.class, args);
+    }
+}
+
 
